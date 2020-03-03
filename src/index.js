8622c5752eb776ab63de708d6c4ec768e7ed57e3
mishashiryakov <mishashiryakov@gmail.com>
2020-03-03 18:04:28 +0300

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+  console.log(n);
+  n += '';
+  n = n.split('').reverse();
+  let res = n.filter(num => /[0-9]/.test(num)).join('');
+  return +res;
+  }
